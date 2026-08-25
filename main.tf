10cb155559c2cfe74aa0ef472a2516c1e6165505
enamoradoX <enamoradoc1989@gmail.com>
2026-08-25 14:00:36 -0400

    Updated Module for newer syntax

--- main.tf
+++ main.tf
@@ -32,16 +32,34 @@ resource "aws_instance" "blog" {
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "6.0.0"
-  name = "blog_new"
+  name    = "blog_new"
 
-  vpc_id = data.aws_vpc.default.
-  
-  ingress_rules = ["http-80-tcp", "https-443-tcp"]
-  ingress_cidr_ipv4 = ["0.0.0.0/0"]
+  vpc_id = data.aws_vpc.default.id # Fixed missing .id attribute
 
-  egress_rules = ["all-all"]
-  egress_cidr_ipv4 = ["0.0.0.0/0"]
+  # Version 6.0.0 maps explicit rules using keys
+  ingress_rules = {
+    http = {
+      from_port   = 80
+      to_port     = 80
+      ip_protocol = "tcp"
+      cidr_ipv4   = "0.0.0.0/0"
+    }
+    https = {
+      from_port   = 443
+      to_port     = 443
+      ip_protocol = "tcp"
+      cidr_ipv4   = "0.0.0.0/0"
+    }
+  }
+
+  egress_rules = {
+    all = {
+      ip_protocol = "-1"
+      cidr_ipv4   = "0.0.0.0/0"
+    }
+  }
 }
+
 
 resource "aws_security_group" "blog" {
   name = "blog"
