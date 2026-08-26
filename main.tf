28667989579f205cbe9a2b6a931dab8c2d673d8d
enamoradoX <enamoradoc1989@gmail.com>
2026-08-26 08:48:46 -0400

    Fix attempt one for tutorial

--- main.tf
+++ main.tf
@@ -27,16 +27,16 @@ module "blog_vpc" {
   enable_nat_gateway = true
 
   tags = {
-    Terraform = "true"
+    Terraform   = "true"
     Environment = "dev"
   }
 }
 
 resource "aws_instance" "blog" {
-  ami                    = data.aws_ami.app_ami.id
-  instance_type          = var.instance_type
+  ami           = data.aws_ami.app_ami.id
+  instance_type = var.instance_type
 
-  vpc_security_group_ids = [module.blog_sg.security_group_id]
+  vpc_security_group_ids = [module.blog_sg.id]
 
   subnet_id = module.blog_vpc.public_subnets[0]
 
@@ -52,7 +52,6 @@ module "blog_sg" {
 
   vpc_id = module.blog_vpc.vpc_id
 
-  # Version 6.0.0 maps explicit rules using keys
   ingress_rules = {
     http = {
       from_port   = 80
@@ -76,40 +75,37 @@ module "blog_sg" {
   }
 }
 
-
 resource "aws_security_group" "blog" {
-  name = "blog"
-  tags = {
+  name   = "blog"
+  tags   = {
     Terraform = "true"
   }
-  vpc_id = data.aws_vpc.default.id
+  vpc_id = module.blog_vpc.vpc_id
 }
 
 resource "aws_security_group_rule" "blog_http_in" {
-  type        = "ingress"
-  from_port   = 80
-  to_port     = 80
-  protocol    = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]
+  type              = "ingress"
+  from_port         = 80
+  to_port           = 80
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
   security_group_id = aws_security_group.blog.id
 }
-
 
 resource "aws_security_group_rule" "blog_https_in" {
-  type        = "ingress"
-  from_port   = 443
-  to_port     = 443
-  protocol    = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]
+  type              = "ingress"
+  from_port         = 443
+  to_port           = 443
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
   security_group_id = aws_security_group.blog.id
 }
 
-
 resource "aws_security_group_rule" "blog_everything_out" {
-  type        = "egress"
-  from_port   = 0
-  to_port     = 0
-  protocol    = "-1"
-  cidr_blocks = ["0.0.0.0/0"]
+  type              = "egress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  cidr_blocks       = ["0.0.0.0/0"]
   security_group_id = aws_security_group.blog.id
 }
